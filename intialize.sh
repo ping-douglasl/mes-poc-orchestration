3352c1bcc0634213ff90e83e3df92c12afc56710
Douglas Linsmeyer <douglasl@ping.com>
2023-11-06 15:38:37 -0700

    Adds check to see if component projects are already installed.

--- intialize.sh
+++ intialize.sh
@@ -1,8 +1,10 @@
-# Clone database repo
-git clone git@github.com:ping-douglasl/mes-poc-database.git database
+if ! [ -d "database" ]; then
+    git clone git@github.com:ping-douglasl/mes-poc-database.git database
+fi
 
-# Clone the dotnet app repo
-git clone git@github.com:ping-douglasl/mes-poc-dotnet-app.git api
+if ! [ -d "api" ]; then
+    git clone git@github.com:ping-douglasl/mes-poc-dotnet-app.git api
+fi
 
 # Run the docker-compose which should reference the Dockerfiles from the sub-projects
 docker-compose up
