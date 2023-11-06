ddd5d7af0d96e7f6c5ba8ae1cb7ae7ecd7e212ee
Douglas Linsmeyer <douglasl@ping.com>
2023-11-06 13:07:30 -0700

    Initial commit.

---
+++ intialize.sh
@@ -0,0 +1,7 @@
+# Go clone the repo for the projects associated with this and put them into this folder...
+
+# Clone infrastructure repo
+
+# Clone the dotnet app repo
+
+# Run the docker-compose which should reference the Dockerfiles from the sub-projects
