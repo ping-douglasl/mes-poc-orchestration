8519802fd43e0ad07365ff59afe6c6b033a6c4b6
Douglas Linsmeyer <douglasl@ping.com>
2023-11-08 14:44:55 -0700

    Revises folder names.

--- init.sh
+++ init.sh
@@ -2,19 +2,19 @@
 docker-compose down --rmi --volumes
 
 # Clean the API project
-rm -rf ./api
+rm -rf ./mes-poc-dotnet-app
 
 # Clean the Database project
-rm -rf ./database
-
-# Clone the Database project repo
-if ! [ -d "database" ]; then
-    git clone git@github.com:ping-douglasl/mes-poc-database.git database
-fi
+rm -rf ./mes-poc-database
 
 # Clone the API project repo
-if ! [ -d "api" ]; then
-    git clone git@github.com:ping-douglasl/mes-poc-dotnet-app.git api
+if ! [ -d "mes-poc-dotnet-app" ]; then
+    git clone git@github.com:ping-douglasl/mes-poc-dotnet-app.git
+fi
+
+# Clone the Database project repo
+if ! [ -d "mes-poc-database" ]; then
+    git clone git@github.com:ping-douglasl/mes-poc-database.git
 fi
 
 # Run the docker-compose which should reference the Dockerfiles from the sub-projects
