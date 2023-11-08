85d8d304b54ad8569ecd27a82ad18ad054ddcc1f
Douglas Linsmeyer <douglasl@ping.com>
2023-11-08 12:34:13 -0700

    Revises environment variables.

--- init.sh
+++ init.sh
@@ -1,7 +1,18 @@
+# Shut down any Docker containers running for this project, remove their images and delete their volumes
+docker-compose down --rmi --volumes
+
+# Clean the API project
+rm -rf ./api
+
+# Clean the Database project
+rm -rf ./database
+
+# Clone the Database project repo
 if ! [ -d "database" ]; then
     git clone git@github.com:ping-douglasl/mes-poc-database.git database
 fi
 
+# Clone the API project repo
 if ! [ -d "api" ]; then
     git clone git@github.com:ping-douglasl/mes-poc-dotnet-app.git api
 fi
