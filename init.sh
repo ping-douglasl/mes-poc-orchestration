c69c497a8fe651bd239192800375ffef34b1cd58
Douglas Linsmeyer <douglinsmeyer@gmail.com>
2023-11-13 12:08:45 -0700

    Refactors to deploy locally using Kubernetes instead of Docker Compose.

--- init.sh
+++ init.sh
@@ -1,9 +1,35 @@
-# Clone the API project repo
-if ! [ -d "mes-poc-dotnet-app" ]; then
-    git clone git@github.com:ping-douglasl/mes-poc-dotnet-app.git
-fi
+#!/bin/bash
 
-# Clone the Database project repo
-if ! [ -d "mes-poc-database" ]; then
-    git clone git@github.com:ping-douglasl/mes-poc-database.git
+kubectl delete deployments --all -n default
+
+# Check if the secret exists
+SECRET_NAME="mes-system-secrets"
+SECRET_DATA_FILE=".secret"
+kubectl get secret "$SECRET_NAME" >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+  kubectl delete secret "$SECRET_NAME"
 fi
+kubectl create secret generic "$SECRET_NAME" --from-env-file="$SECRET_DATA_FILE"
+
+# Check if the config map exists
+CONFIG_MAP_NAME="mes-system-env"
+CONFIG_MAP_DATA_FILE=".config"
+kubectl get configmap "$CONFIG_MAP_NAME" >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+  kubectl delete configmap "$CONFIG_MAP_NAME"
+fi
+kubectl create configmap "$CONFIG_MAP_NAME" --from-env-file="$CONFIG_MAP_DATA_FILE"
+
+# Set up the "mes-poc-database" component.
+if ! [ -d "mes-poc-database" ]; then
+  git clone git@github.com:ping-douglasl/mes-poc-database.git
+fi
+./mes-poc-database/build.sh
+./mes-poc-database/deploy.sh
+
+# Set up the "mes-poc-dotnet-app" component.
+if ! [ -d "mes-poc-dotnet-app" ]; then
+  git clone git@github.com:ping-douglasl/mes-poc-dotnet-app.gitX
+fi
+./mes-poc-dotnet-app/build.sh
+./mes-poc-dotnet-app/deploy.sh
